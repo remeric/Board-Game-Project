cb1a2547a237354abeb5ade265da4895700f5210
remeric <jshodges@hotmail.com>
2021-10-13 13:00:06 -0500

    variables test on Azure Devops

--- terraform/aws-ecs/variables.tf
+++ terraform/aws-ecs/variables.tf
@@ -1,5 +1,5 @@
-variable "client_id" {}
-variable "client_secret" {}
+#variable "client_id" {}
+#variable "client_secret" {}
 variable "ssh_public_key" {
   default = "C:/Terraform/aws/aws_keys/default-ec2.pem"
 }
